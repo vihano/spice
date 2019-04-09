c11ac0bc39384e971e1f82e994f250601c52bae4
Jose Vihano <vihano@gmail.com>
2019-04-09 13:05:45 +0530

    Update info.js

--- info.js
+++ info.js
@@ -1,1 +1,3 @@
-var zohouser = {"FULL_NAME":"Jose\\x20Vihano","DISPLAY_NAME":"Jose\\x20Vihano","DC_INFO":"us"};
+var account = function(){
+  return {"FULL_NAME":"Jose\\x20Vihano","DISPLAY_NAME":"Jose\\x20Vihano","DC_INFO":"us"};
+}
